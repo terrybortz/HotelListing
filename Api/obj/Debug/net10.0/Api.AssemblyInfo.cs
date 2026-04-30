bdf877d09daa37a19a38e0a52deb11bdc6bfb4d3
terrybortz <terrybortz@gmail.com>
2026-05-01 01:02:19 +0200

    gitignore related changes

--- Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2e923719ac0f49ee81f77d28d62e0fe23fb3e92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3378df43719ca7afa8c4ad37e225fa6667a102c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
